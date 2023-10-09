6d9993435791b93e1ee4e8cba27020afb81dd0f4
Lucas J <lucas@wifi-10-40-177-0.wifi.berkeley.edu>
2023-10-09 13:53:11 -0700

    start

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Square() {
+  return (
+    <>
+      <button className="square">X</button>;
+      <button className="square">X</button>;
+      <button className="square">X</button>;
+      <button className="square">X</button>;
+      <button className="square">X</button>;
+      <button className="square">X</button>;
+      <button className="square">X</button>;
+      <button className="square">X</button>;
+      <button className="square">X</button>;
+    </>
+  );
+}
